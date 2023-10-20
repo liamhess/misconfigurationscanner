bfce0a70ef9568f0ba646e69be24afa1113474e2
Liro08 <liam.rossel@gmail.com>
2023-10-20 18:02:30 +0200

    ip angepasst

--- mail.py
+++ mail.py
@@ -17,7 +17,6 @@ def send_email_alerts(ips, ports):
     # Anmeldung beim E-Mail-Konto
     server.login(sender_email, password)
 
-    ip_text = ', '.join(ips)  # Konvertiere die Liste der IPs zu einem kommagetrennten String
 
     # Erstellen der E-Mail-Nachricht
     message = MIMEMultipart()
@@ -26,7 +25,7 @@ def send_email_alerts(ips, ports):
     message['To'] = receiver_email
     message['Subject'] = 'Securitywarning for Servers with the IPs:'
 
-    body = f'This is a warning, on servers with the following IPs: {ip_text}, the following ports are open: {ports}! \n\n The Admin interface is open, please fix this immediately! \n\n This poses a major risk to our security \n\n On this page you will learn how to close the port:\nhttps://www.acunetix.com/blog/articles/close-unused-open-ports/\n'
+    body = f'This is a warning, on servers with the following IPs: {ips}, the following ports are open: {ports}! \n\n The Admin interface is open, please fix this immediately! \n\n This poses a major risk to our security \n\n On this page you will learn how to close the port:\nhttps://www.acunetix.com/blog/articles/close-unused-open-ports/\n'
     message.attach(MIMEText(body, 'plain'))
 
     # E-Mail senden
@@ -37,8 +36,8 @@ def send_email_alerts(ips, ports):
 
 # E-Mail-Konfiguration
 
-#ips = ['127.0.0.1', '192.168.1.1', '10.0.0.1']
-#ports = ['44', '10000', '25']
+ips = '127.0.0.1'
+ports = ['44', '10000', '25']
 
 # Funktion aufrufen, um E-Mails zu senden
-send_email_alerts(ip_text, ports)
+send_email_alerts(ips, ports)
