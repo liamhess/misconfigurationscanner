c6bf0695a193404128c5a4076a6a99d21625c46b
Mike Ditton <mike@d.ditton.ch>
2023-10-20 19:04:18 +0200

    changed merge conflict

--- mail.py
+++ mail.py
@@ -1,32 +1,37 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-import getpass
+from decouple import config
+import hashlib
+
+def hash_password(password):
+    # Hier wird das Passwort mit SHA-256 gehasht, Sie können eine andere Methode verwenden
+    sha256 = hashlib.sha256()
+    sha256.update(password.encode('utf-8'))
+    return sha256.hexdigest()
 
 def send_email_alerts(ips, ports):
-    sender_email = 'liro08@gmx.ch'
-    receiver_email = 'simeon.schaer1@gmail.com'
-    password = getpass.getpass('Bitte gib dein E-Mail Passwort ein: ')
-    
-    # Verbindung zum E-Mail-Server herstellen
-    smtp_server = 'mail.gmx.net'
-    smtp_port = 587
-    server = smtplib.SMTP(smtp_server, smtp_port)
-    server.starttls()  # TLS-Verschlüsselung aktivieren
-
-    # Anmeldung beim E-Mail-Konto
-    server.login(sender_email, password)
-
+    sender_email = config('SENDER_EMAIL')
+    receiver_email = config('RECEIVER_EMAIL')
+    password = config('PASSWORD_HASHED')
 
     # Erstellen der E-Mail-Nachricht
     message = MIMEMultipart()
-    #port_text = ', '.join(ports)  # Konvertiere die Liste der Ports zu einem kommagetrennten String
     message['From'] = sender_email
     message['To'] = receiver_email
     message['Subject'] = 'Securitywarning for Servers with the IPs:'
 
     body = f'This is a warning, on servers with the following IPs: {ips}, the following ports are open: {ports}! \n\n The Admin interface is open, please fix this immediately! \n\n This poses a major risk to our security \n\n On this page you will learn how to close the port:\nhttps://www.acunetix.com/blog/articles/close-unused-open-ports/\n'
     message.attach(MIMEText(body, 'plain'))
+
+    # Verbindung zum E-Mail-Server herstellen
+    smtp_server = config('SMTP_SERVER')
+    smtp_port = config('SMTP_PORT')
+    server = smtplib.SMTP(smtp_server, smtp_port)
+    server.starttls()  # TLS-Verschlüsselung aktivieren
+
+    # Anmeldung beim E-Mail-Konto mit dem gehashten Passwort
+    server.login(sender_email, password)
 
     # E-Mail senden
     server.sendmail(sender_email, receiver_email, message.as_string())
@@ -35,8 +40,8 @@ def send_email_alerts(ips, ports):
     server.quit()
 
 # E-Mail-Konfiguration
-
 ips = '127.0.0.1'
 ports = ['44', '10000', '25']
 
-
+# E-Mail senden
+send_email_alerts(ips, ports)
