69acbf9db6fbf365e3d789806364c70b9660d077
Liro08 <liam.rossel@gmail.com>
2023-10-20 16:46:04 +0200

    mail mit ports

--- mail.py
+++ mail.py
@@ -3,8 +3,11 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import getpass
 
-def send_email_alerts(sender_email, receiver_email, password, ip_ports):
-    # Verbindung zum E-Mail-Server herstellen
+def send_email_alerts(ips, ports):
+    sender_email = 'liro08@gmx.ch'
+    receiver_email = 'simeon.schaer1@gmail.com'
+    password = getpass.getpass('Bitte gib dein E-Mail Passwort ein: ')
+# Verbindung zum E-Mail-Server herstellen
     smtp_server = 'mail.gmx.net'
     smtp_port = 587
     server = smtplib.SMTP(smtp_server, smtp_port)
@@ -13,36 +16,28 @@ def send_email_alerts(sender_email, receiver_email, password, ip_ports):
     # Anmeldung beim E-Mail-Konto
     server.login(sender_email, password)
 
-    # Erstelle eine leere Menge, um zu überprüfen, ob eine IP bereits verarbeitet wurde
-    processed_ips = set()
-
-    for ip, port in ip_ports:
-        if ip not in processed_ips:
+    for ip in ips:
+        for port in ports:
             # Erstellen der E-Mail-Nachricht
             message = MIMEMultipart()
             message['From'] = sender_email
             message['To'] = receiver_email
-            message['Subject'] = f'Offene Ports bei {ip}'
+            message['Subject'] = 'Warning!'
 
-            # E-Mail-Inhalt mit formatierter IP-Adresse und Liste der offenen Ports
-            open_ports = [p for i, p in ip_ports if i == ip]
-            body = f'Dies ist ein Notfall, weil bei dem Server {ip} die folgenden Ports offen sind: {", ".join(open_ports)}'
+            port_text = ', '.join(ports)  # Konvertiere die Liste der Ports zu einem kommagetrennten String
+            body = f'This is a warning, on server {ip} the following ports are open: {port_text}! \n\n The Admin interface is open, please fix this immediately! \n\n This poses a major risk to our security \n\n On this page you will learn how to close the port:\nhttps://www.acunetix.com/blog/articles/close-unused-open-ports/\n'
             message.attach(MIMEText(body, 'plain'))
 
             # E-Mail senden
             server.sendmail(sender_email, receiver_email, message.as_string())
 
-            # Füge die verarbeitete IP zur Menge hinzu
-            processed_ips.add(ip)
-
     # Verbindung zum E-Mail-Server beenden
     server.quit()
 
 # E-Mail-Konfiguration
-sender_email = 'liro08@gmx.ch'
-receiver_email = 'simeon.schaer1@gmail.com'
-password = getpass.getpass('Bitte gib dein E-Mail Passwort ein: ')
-ip_ports = [('127.0.0.1', '44'), ('192.168.1.1', '10000'), ('10.0.0.1', '10000')]
+
+ips = ['127.0.0.1', '192.168.1.1', '10.0.0.1']
+ports = ['44', '10000', '25']
 
 # Funktion aufrufen, um E-Mails zu senden
-send_email_alerts(sender_email, receiver_email, password, ip_ports)
+send_email_alerts(ips, ports)
