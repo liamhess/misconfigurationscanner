d3b010c0de50f00d0549952297fa522b29b9bf7d
Mike Ditton <mike@d.ditton.ch>
2023-10-21 14:46:15 +0200

    removed mail send

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def check_admin_interface(ip, port):
 async def check_ip_and_port(ip, port):
     if await check_port(ip, port):
         print(f"Port {port} is open on {ip}")
-        mail.send_email_alerts(ip, port)
+        # mail.send_email_alerts(ip, port)
         return {ip: port, "open": True}
     else:
         print(f"Port {port} is not open on {ip}")
